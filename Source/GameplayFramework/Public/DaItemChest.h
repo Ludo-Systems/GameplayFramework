45fd2df8aaf2bb4e991f7d1229963dcbe083c041
Filip Iliescu <Ancient23@users.noreply.github.com>
2024-12-30 21:41:15 -0800

    Add a delegate method to item chest to notify when opened and closed

--- Source/GameplayFramework/Public/DaItemChest.h
+++ Source/GameplayFramework/Public/DaItemChest.h
@@ -7,6 +7,8 @@
 #include "DaInteractableInterface.h"
 #include "DaItemChest.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLidOpenedStateChanged, bool, IsOpened);
+
 UCLASS()
 class GAMEPLAYFRAMEWORK_API ADaItemChest : public AActor, public IDaInteractableInterface
 {
@@ -16,18 +18,17 @@ public:
 	// Sets default values for this actor's properties
 	ADaItemChest();
 
+	// IDaInteractableInterface
 	virtual void Interact_Implementation(APawn* InstigatorPawn) override;
-
 	virtual void SecondaryInteract_Implementation(APawn* InstigatorPawn) override;
-	
 	virtual FText GetInteractText_Implementation(APawn* InstigatorPawn) override;
-	
 	virtual void OnActorLoaded_Implementation() override;
-
 	virtual void HighlightActor_Implementation() override;
-
 	virtual void UnHighlightActor_Implementation() override;
 
+	UPROPERTY(BlueprintAssignable, Category="ItemChest")
+	FOnLidOpenedStateChanged OnLidOpenedStateChanged;
+	
 protected:
 
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
@@ -36,7 +37,7 @@ protected:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
 	TObjectPtr<UStaticMeshComponent> LidMesh;
 	
-	UPROPERTY(EditAnywhere, Category="Props")
+	UPROPERTY(EditAnywhere, Category="ItemChest")
 	float TargetPitch;
 
 	UPROPERTY(ReplicatedUsing="OnRep_LidOpened", BlueprintReadOnly, SaveGame)
