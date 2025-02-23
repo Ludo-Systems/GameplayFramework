544920cc862fbe9db42a5a6586791cc0e99def63
Ancient23 <Ancient23@users.noreply.github.com>
2025-02-22 22:05:28 -0800

    Use FGameplayTagBlueprintPropertyMap on tagged widget as a way to bind elements to gameplay tags granted to the player via playerstate

--- Source/GameplayFramework/Private/UI/DaTaggedWidget.cpp
+++ Source/GameplayFramework/Private/UI/DaTaggedWidget.cpp
@@ -3,6 +3,9 @@
 
 #include "UI/DaTaggedWidget.h"
 
+#include "DaPlayerState.h"
+#include "AbilitySystem/DaAbilitySystemComponent.h"
+
 UDaTaggedWidget::UDaTaggedWidget(const FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
 {
@@ -15,65 +18,10 @@ void UDaTaggedWidget::NativeConstruct()
 	if (!IsDesignTime())
 	{
 		// Listen for tag changes on our hidden tags
-		//@TODO: That thing I said
-
-		// Set our initial visibility value (checking the tags, etc...)
-		SetVisibility(GetVisibility());
-	}
-}
-
-void UDaTaggedWidget::NativeDestruct()
-{
-	if (!IsDesignTime())
-	{
-		//@TODO: Stop listening for tag changes
-	}
-	
-	Super::NativeDestruct();
-}
-
-void UDaTaggedWidget::SetVisibility(ESlateVisibility InVisibility)
-{
-#if WITH_EDITORONLY_DATA
-	if (IsDesignTime())
-	{
-		Super::SetVisibility(InVisibility);
-		return;
-	}
-#endif
-
-	// Remember what the caller requested; even if we're currently being
-	// suppressed by a tag we should respect this call when we're done
-	bWantsToBeVisible = ConvertSerializedVisibilityToRuntime(InVisibility).IsVisible();
-	if (bWantsToBeVisible)
-	{
-		ShownVisibility = InVisibility;
-	}
-	else
-	{
-		HiddenVisibility = InVisibility;
-	}
-
-	//@TODO: Foo->HasAnyTags(HiddenByTags);
-	const bool bHasHiddenTags = false;
-
-	// Actually apply the visibility
-	const ESlateVisibility DesiredVisibility = (bWantsToBeVisible && !bHasHiddenTags) ? ShownVisibility : HiddenVisibility;
-	if (GetVisibility() != DesiredVisibility)
-	{
-		Super::SetVisibility(DesiredVisibility);
-	}
-}
-
-void UDaTaggedWidget::OnWatchedTagsChanged()
-{
-	//@TODO: Foo->HasAnyTags(HiddenByTags);
-	const bool bHasHiddenTags = false;
-
-	// Actually apply the visibility
-	const ESlateVisibility DesiredVisibility = (bWantsToBeVisible && !bHasHiddenTags) ? ShownVisibility : HiddenVisibility;
-	if (GetVisibility() != DesiredVisibility)
-	{
-		Super::SetVisibility(DesiredVisibility);
+		if (ADaPlayerState* PlayerState = Cast<ADaPlayerState>(GetOwningPlayerState()))
+		{
+			UDaAbilitySystemComponent* ASC = PlayerState->GetDaAbilitySystemComponent();
+			GameplayTagPropertyMap.Initialize(this, ASC);
+		}
 	}
 }
--- Source/GameplayFramework/Public/UI/DaTaggedWidget.h
+++ Source/GameplayFramework/Public/UI/DaTaggedWidget.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "CommonUserWidget.h"
-#include "GameplayTagContainer.h"
+#include "GameplayEffectTypes.h"
 #include "DaTaggedWidget.generated.h"
 
 /**
@@ -18,31 +18,13 @@ class GAMEPLAYFRAMEWORK_API UDaTaggedWidget : public UCommonUserWidget
 public:
 	UDaTaggedWidget(const FObjectInitializer& ObjectInitializer);
 
-	//~UWidget interface
-	virtual void SetVisibility(ESlateVisibility InVisibility) override;
-	//~End of UWidget interface
-
 	//~UUserWidget interface
 	virtual void NativeConstruct() override;
-	virtual void NativeDestruct() override;
 	//~End of UUserWidget interface
 
 protected:
-	/** If the owning player has any of these tags, this widget will be hidden (using HiddenVisibility) */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "HUD")
-	FGameplayTagContainer HiddenByTags;
 
-	/** The visibility to use when this widget is shown (not hidden by gameplay tags). */
-	UPROPERTY(EditAnywhere, Category = "HUD")
-	ESlateVisibility ShownVisibility = ESlateVisibility::Visible;
-
-	/** The visibility to use when this widget is hidden by gameplay tags. */
-	UPROPERTY(EditAnywhere, Category = "HUD")
-	ESlateVisibility HiddenVisibility = ESlateVisibility::Collapsed;
-
-	/** Do we want to be visible (ignoring tags)? */
-	bool bWantsToBeVisible = true;
-
-private:
-	void OnWatchedTagsChanged();
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+	
 };
