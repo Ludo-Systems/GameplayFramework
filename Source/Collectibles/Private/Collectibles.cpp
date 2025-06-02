27865a39c1650d1f785486aec7ccdb029205d06c
Filip Iliescu <Ancient23@users.noreply.github.com>
2025-06-02 10:00:14 -0700

    Scaffold collectibles submodule

---
+++ Source/Collectibles/Private/Collectibles.cpp
@@ -0,0 +1,17 @@
+﻿#include "Collectibles.h"
+
+#define LOCTEXT_NAMESPACE "FCollectiblesModule"
+
+void FCollectiblesModule::StartupModule()
+{
+    
+}
+
+void FCollectiblesModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCollectiblesModule, Collectibles)
